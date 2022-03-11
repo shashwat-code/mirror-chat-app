055854a1fca6321a4d2b6119858b5f5c86170dae
shashwat-code <91280518+shashwat-code@users.noreply.github.com>
2022-03-12 00:46:10 +0530

    bugs resolved

--- src/App.js
+++ src/App.js
@@ -2,67 +2,25 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import Avatar from  './Lustro.png';
-
+import moment from 'moment';
 
 
 const App=()=>{
   const[userMessage, setUserMessage] = useState([]);
   const [isDarkModeActive, setIsDarkModeActive] = useState(false)
-  const [savedChat, setSavedChat] = useState();
-     //JSON.stringify(localStorage.getItem('chats'));
-    //setData(savedChats)
+  const [savedChat, setSavedChat] = useState([]);
   const[input,setInput] = useState("");
+  const[isEmpty,setEmpty]=useState(true)
   const [error, setError] =useState(null)
   useEffect(()=>{
-    let savedChats = localStorage.getItem('chats');
-    function parseData(savedChats) {
-
-      if (!savedChats){ console.log("firstif",savedChats) ;return {}};
-      if (typeof savedChats === 'object') {console.log("objif",savedChats);return savedChats};
-      if (typeof savedChats === 'string') {console.log("strtif",savedChats);return JSON.parse(savedChats)};
-      console.log("afterif",savedChats)
-      return {};
-  }
-  savedChats =parseData(savedChats);
-    console.log("savedchats",savedChats)
-    setSavedChat(savedChats);
-    console.log("savedchat", savedChat);
-   /*if(savedChats && savedChats.chatMessage){ Object.keys(savedChats).map((singleChat,index)=>{
-      let copy = [...userMessage];
-      copy = [...copy, { chatID: userMessage.length + 1, chatMessage: singleChat.chatMessage }];
-      console.log("copy in useffect", copy)
-      setUserMessage(copy);
-      //send();
-      return;
-    })}*/
-   
-    //setUserMessage(savedChats);
-  },[])
-
-  /*useEffect(()=>{
-    let copy = [...userMessage];
-    copy = [...copy, { chatID: userMessage.length + 1, chatMessage: data }];
-    setUserMessage(copy);
-  },[data])*/
-
-
-  /*useEffect(() => {
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-      
-    };
-  }, []);
-  
-  const handleBeforeUnload = (e) => {
-    e.preventDefault();
-    let message =
-      "Do you want to save your chat?";
-    e.returnValue = message;
-    return message;
-  };*/
-
-
+    let getChat = localStorage.getItem('chats');
+    let data  = JSON.parse(getChat)
+    if(data===null){
+      setSavedChat([])
+      return
+    }
+    setSavedChat(data);},[])
+  // MODE settings  
   const switchModes = (mode) => {
     if(mode === "light") {
       setIsDarkModeActive(false)
@@ -72,21 +30,23 @@ const App=()=>{
   }
   
   const send = e =>{
+    // if(input.length===0){
+    //   setEmpty(true)
+    //   return
+    // }
+    // setEmpty(true)
     try {
-      // Do something that could throw
       e.preventDefault();
-      console.log("start");
-      let copy = [...userMessage];
-      copy = [...copy, { chatID: userMessage.length + 1, chatMessage: input }];
-      setUserMessage(copy);
-      console.log("copy",copy);
+      let copy = [...savedChat];
+      const date = new moment()
+      const deliveryTime = `${date.get('hour')}:${date.get('minute')}`
+      console.log(deliveryTime)
+      copy = [...copy, { chatID: savedChat.length + 1, chatMessage: input,DeliveryTime: deliveryTime}];
+      setSavedChat(copy);
       setInput("")
-      console.log("in send",userMessage);
-      //localStorage.setItem('chats', userMessage);
-      localStorage.setItem('chats', JSON.stringify(userMessage));
-    } catch (error) {
+      localStorage.setItem('chats', JSON.stringify(savedChat));
+    }catch (error) {
       setError( error );
-      console.log(error);
     }
  
 }
@@ -111,15 +71,19 @@ const App=()=>{
     </div>
     <div  className={isDarkModeActive ? "chatBoxBody dark-chatBoxBody" : "chatBoxBody"}>
     
-    {userMessage.map((message)=>{
-       return<><div  className={isDarkModeActive ? "userMessage dark-userMessage" : "userMessage"}>
-         <div   id='userInputMessage' className={isDarkModeActive ? "message dark-message" : "message"}>
+    {savedChat?savedChat.map((message)=>{
+       return<><div key={message.chatID}  className={isDarkModeActive ? "userMessage dark-userMessage" : "userMessage"}>
+         <div  id='userInputMessage' className={isDarkModeActive ? "message dark-message" : "message"}>
        {message.chatMessage}
-      </div> </div>
-      <div className='chatBotResponse'> 
+      </div>
+        <div>{message.DeliveryTime}</div>
+       </div>
+      <div className='chatBotResponse' key={message.chatID+"12"}> 
         <div  id="chatBotResponseMessage" className={isDarkModeActive ? "message dark-message" : "message"} >
           {message.chatMessage}
-          </div></div></> })}
+          </div>
+          <div>{message.DeliveryTime}</div>
+          </div></> }):<>Loading</>}
     </div>
     
       
@@ -129,7 +93,7 @@ const App=()=>{
     
     <form action="#0" id="user-input-form">
     <input type="text" id="user-input" className={isDarkModeActive ? "userInput dark-userInput" : "userInput"} placeholder="Talk to Luke..."value={input} onChange={e=>{setInput(e.target.value)}}/>
-    <button type='submit' className= {isDarkModeActive ? "sendButton dark-sendButton" : "sendButton"} onClick={send}>Send</button>
+    <button type='submit' disabled={!isEmpty}  className= {isDarkModeActive ? "sendButton dark-sendButton" : "sendButton"} onClick={send}>Send</button>
   </form>
     </div>
   </div>)
